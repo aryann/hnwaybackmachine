cab360fa8c90836b611a032289747d67841bfccc
Aryan Naraghi <aryan.naraghi@gmail.com>
2021-01-01 13:36:02 -0800

    Updates the update_db.py script to use Python's asyncio.

--- update_db.py
+++ update_db.py
@@ -1,31 +1,25 @@
+import asyncio
 import logging
-import multiprocessing
-import multiprocessing.pool
-import multiprocessing.context
 import requests
 import sqlite3
 import sys
 
 
-def find_gaps(in_queue, db, high_water_mark):
-    conn = sqlite3.connect(db)
+async def find_gaps(conn, high_water_mark, queue):
+    rows = conn.execute('SELECT id FROM Items ORDER BY id')
     try:
-        rows = conn.execute('SELECT id FROM Items ORDER BY id')
-        try:
-            next_id = next(rows)[0]
-        except StopIteration:
-            next_id = None
+        next_id = next(rows)[0]
+    except StopIteration:
+        next_id = None
 
-        for id in range(1, high_water_mark):
-            if next_id == id:
-                try:
-                    next_id = next(rows)[0]
-                except StopIteration:
-                    next_id = None
-            else:
-                in_queue.put(id)
-    finally:
-        conn.close()
+    for id in range(1, high_water_mark):
+        if next_id == id:
+            try:
+                next_id = next(rows)[0]
+            except StopIteration:
+                next_id = None
+        else:
+            await queue.put(id)
 
 
 def find_remote_high_water_mark():
@@ -34,72 +28,69 @@ def find_remote_high_water_mark():
     return int(response.text)
 
 
-def get_item(in_queue, out_queue):
-    while True:
-        next = in_queue.get()
-        if next is None:
-            return
-        response = requests.get(
-            f'https://hacker-news.firebaseio.com/v0/item/{next}.json')
-        out_queue.put(response.json())
+async def get_item(id):
+    loop = asyncio.get_event_loop()
+    future = loop.run_in_executor(
+        None, requests.get,
+        f'https://hacker-news.firebaseio.com/v0/item/{id}.json')
+    response = await future
+    return response.json()
 
 
-def save_items(queue, db):
-    conn = sqlite3.connect(db)
-    try:
-        while True:
-            item = queue.get()
-            if item is None:
-                return
-
-            if item.get('type') == 'story':
-                conn.execute("""\
+def save_item(conn, item):
+    if item.get('type') == 'story':
+        conn.execute("""\
                     INSERT INTO Items
                     (id, type, by, time, url, score, title)
                     VALUES
                     (?, "story", ?, ?, ?, ?, ?)""",
-                             (item['id'], item.get('by'), item.get('time'), item.get('url'),
-                              item.get('score'), item.get('title')))
-            else:
-                conn.execute("""\
+                     (item['id'], item.get('by'), item.get('time'), item.get('url'),
+                      item.get('score'), item.get('title')))
+    else:
+        conn.execute("""\
                     INSERT INTO Items
                     (id, type, time)
                     VALUES
                     (?, ?, ?)""",
-                             (item['id'], item.get('type'), item.get('time')))
+                     (item['id'], item.get('type'), item.get('time')))
 
-            conn.commit()
-            logging.info('committed item with id %d', item['id'])
-    finally:
-        conn.close()
+    conn.commit()
+
+
+async def get_and_save_item(conn, queue, task_id):
+    while True:
+        id = await queue.get()
+        item = await get_item(id)
+        save_item(conn, item)
+        logging.info('task %4d: committed item with id %d',
+                     task_id, item['id'])
+        queue.task_done()
+
+
+async def run(conn, num_consumers):
+    remote_high_water_mark = find_remote_high_water_mark()
+    logging.info('remote high water mark: %d', remote_high_water_mark)
+    queue = asyncio.Queue(maxsize=10000)
+
+    producer = asyncio.create_task(
+        find_gaps(conn, remote_high_water_mark, queue))
+    consumers = [asyncio.create_task(
+        get_and_save_item(conn, queue, task_id))
+        for task_id in range(num_consumers)]
+
+    await asyncio.gather(producer)
+    await queue.join()
+    for consumer in consumers:
+        consumer.cancel()
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     db = sys.argv[1]
-    request_threads = int(sys.argv[2])
+    num_consumers = int(sys.argv[2])
 
-    remote_high_water_mark = find_remote_high_water_mark()
-    logging.info('remote high water mark: %d', remote_high_water_mark)
-
-    in_queue = multiprocessing.Queue(maxsize=request_threads * 100)
-    out_queue = multiprocessing.Queue(maxsize=request_threads * 10)
-
-    with multiprocessing.pool.ThreadPool(processes=request_threads + 2) as pool:
-        callbacks = []
-
-        callbacks.append(pool.apply_async(
-            find_gaps, (in_queue, db, remote_high_water_mark)))
-        callbacks.append(pool.apply_async(
-            save_items, (out_queue, db)))
-
-        for _ in range(request_threads):
-            callbacks.append(pool.apply_async(
-                get_item, (in_queue, out_queue)))
-
-        while True:
-            for callback in callbacks:
-                try:
-                    callback.get(timeout=0.1)
-                except multiprocessing.context.TimeoutError:
-                    continue
+    conn = sqlite3.connect(db)
+    try:
+        asyncio.run(run(conn, num_consumers))
+    finally:
+        conn.close()
